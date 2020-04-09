a54bb906bca016a12fb4b58508f28ae2d98d5737
DanielSzym <d.szymkowiak@gmx.net>
2020-04-09 19:48:51 +0200

    challenges page

--- assets/js/global.js
+++ assets/js/global.js
@@ -26,7 +26,7 @@ function closeNav() {
 }
 
 function resetMenuBar() {
-    console.log('resetMenuBar() called')
+    // console.log('resetMenuBar() called')
     var menuItems = document.querySelectorAll('.menu-item')
 
     menuItems.forEach(function (item) {
@@ -35,12 +35,41 @@ function resetMenuBar() {
 }
 
 function activateMenuItem(menuItem) {
-    console.log('activateMenuItem() called')
+    // console.log('activateMenuItem() called')
     let menuItems = document.querySelectorAll(menuItem);
     // var menuItemsArray = Array.prototype.slice.call(menuItems);
-    console.log(menuItems)
+    // console.log(menuItems)
 
     menuItems.forEach(function (item) {
         item.classList.add('current-menu')
     });
+
+}
+
+document.getElementsByClassName('challenge-content')[0]
+        .addEventListener('click', function (event) {
+            // do something
+        });
+// challenges: show selected
+function changeChallenge(element) {
+    // add filter to all images that are in div class <category-image>
+    for (let item of document.getElementsByClassName('category')) {
+        item.classList.add('not-selected')
+        item.getElementsByTagName('img')[0].classList.add('img-greyed')
+        item.getElementsByTagName('img')[0].classList.remove('img-selected')
+        
+    }
+    element.classList.remove('not-selected')
+    element.getElementsByTagName('img')[0].classList.remove('img-greyed')
+    element.getElementsByTagName('img')[0].classList.add('img-selected')
+        
+    // to all sections with class <challenge-content> add d-none
+    for (let item of document.getElementsByClassName('challenge-content')) {
+        item.classList.add('d-none')
+    }
+    // remove d-none of element with id in attribute data-target-id
+    console.log('element', element)
+    let target_id = element.getAttribute('data-target-id')
+    console.log('target_id', target_id)
+    document.getElementById(target_id).classList.remove('d-none')
 }
