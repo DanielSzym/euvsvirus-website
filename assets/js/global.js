31d4667750ea5fe0f3cff7d50f443a56ac25dc24
DanielSzym <d.szymkowiak@gmx.net>
2020-04-11 19:43:03 +0200

    IE compatibility in js file

--- assets/js/global.js
+++ assets/js/global.js
@@ -83,17 +83,46 @@ window.onload = function () {
 }
 
 
+// challenges: show selected
+function changeChallenge(element) {
+  // add filter to all images that are in div class <category-image>
+  // IE workaround
+  let html_categories = document.getElementsByClassName('category');
+  for (var i = 0; i < html_categories.length; i++) {
+    html_categories[i].classList.add('not-selected');
+    html_categories[i].getElementsByTagName('img')[0].classList.add('img-greyed');
+    html_categories[i].getElementsByTagName('img')[0].classList.remove('img-selected');
+  }
+  element.classList.remove('not-selected');
+
+  element.getElementsByTagName('img')[0].classList.remove('img-greyed');
+  element.getElementsByTagName('img')[0].classList.add('img-selected');
+      
+  // to all sections with class <challenge-content> add d-none
+  // IE workaround
+  let html_challenge_content = document.getElementsByClassName('challenge-content');
+  for (var i = 0; i < html_challenge_content.length; i++) {
+    html_challenge_content[i].classList.add('d-none');
+  }
+  // remove d-none of element with id in attribute data-target-id
+  let target_id = element.getAttribute('data-target-id')
+  document.getElementById(target_id).classList.remove('d-none')
+  window.history.pushState("object or string", "Title", "#" + target_id);
+}
+
+
+
 // 'activate' menu item based on url
 let url = window.location.href
-if (url.includes('challenges')) { 
+if (url.indexOf('challenges') > -1) { 
     resetMenuBar(); 
     activateMenuItem('.menu-challenges');
-    if(url.includes('#health')) { changeChallenge(document.getElementById('challenges-menu-health'))}
-    else if(url.includes('#business')) { changeChallenge(document.getElementById('challenges-menu-business'))}
-    else if(url.includes('#social')) { changeChallenge(document.getElementById('challenges-menu-social'))}
-    else if(url.includes('#remote')) { changeChallenge(document.getElementById('challenges-menu-education'))}
-    else if(url.includes('#finance')) { changeChallenge(document.getElementById('challenges-menu-finance'))}
-    else if(url.includes('#other')) { changeChallenge(document.getElementById('challenges-menu-other'))}
+    if(url.indexOf('#health') > -1) { changeChallenge(document.getElementById('challenges-menu-health'))}
+    else if(url.indexOf('#business') > -1) { changeChallenge(document.getElementById('challenges-menu-business'))}
+    else if(url.indexOf('#social') > -1) { changeChallenge(document.getElementById('challenges-menu-social'))}
+    else if(url.indexOf('#remote') > -1) { changeChallenge(document.getElementById('challenges-menu-education'))}
+    else if(url.indexOf('#finance') > -1) { changeChallenge(document.getElementById('challenges-menu-finance'))}
+    else if(url.indexOf('#other') > -1) { changeChallenge(document.getElementById('challenges-menu-other'))}
 
     // dont jump to anchor on challenges page
     if (location.hash) {
@@ -102,15 +131,15 @@ if (url.includes('challenges')) {
         }, 1);
       }
 }
-else if (url.includes('agenda')) { resetMenuBar(); activateMenuItem('.menu-agenda') }
-else if (url.includes('resources')) { resetMenuBar(); activateMenuItem('.menu-resources') }
-else if (url.includes('press')) { resetMenuBar(); activateMenuItem('.menu-press') }
-else if (url.includes('team')) { resetMenuBar(); activateMenuItem('.menu-team') }
-else if (url.includes('faq')) { resetMenuBar(); activateMenuItem('.menu-faq') }
-else if (url.includes('terms')) { resetMenuBar(); }
-else if (url.includes('privacy')) { resetMenuBar(); }
-else if (url.includes('terms')) { resetMenuBar(); }
-else if (url.includes('imprint')) { resetMenuBar(); }
+else if (url.indexOf('agenda') > -1) { resetMenuBar(); activateMenuItem('.menu-agenda') }
+else if (url.indexOf('resources') > -1) { resetMenuBar(); activateMenuItem('.menu-resources') }
+else if (url.indexOf('press') > -1) { resetMenuBar(); activateMenuItem('.menu-press') }
+else if (url.indexOf('team') > -1) { resetMenuBar(); activateMenuItem('.menu-team') }
+else if (url.indexOf('faq') > -1) { resetMenuBar(); activateMenuItem('.menu-faq') }
+else if (url.indexOf('terms') > -1) { resetMenuBar(); }
+else if (url.indexOf('privacy') > -1) { resetMenuBar(); }
+else if (url.indexOf('terms') > -1) { resetMenuBar(); }
+else if (url.indexOf('imprint') > -1) { resetMenuBar(); }
 else { resetMenuBar(); activateMenuItem('.menu-hackathon'); }
 
 // mobile menu
@@ -127,55 +156,27 @@ function closeNav() {
 }
 
 function resetMenuBar() {
-    // console.log('resetMenuBar() called')
     var menuItems = document.querySelectorAll('.menu-item')
 
-    menuItems.forEach(function (item) {
-        item.classList.remove('current-menu')
-    });
+    // IE workaround
+    for (var i = 0; i < menuItems.length; i++) {
+        menuItems[i].classList.remove('current-menu');
+    }
+    
 }
 
 function activateMenuItem(menuItem) {
-    // console.log('activateMenuItem() called')
     let menuItems = document.querySelectorAll(menuItem);
-    // var menuItemsArray = Array.prototype.slice.call(menuItems);
-    // console.log(menuItems)
 
-    menuItems.forEach(function (item) {
-        item.classList.add('current-menu')
-    });
-
+    // IE workaround
+    for (var i = 0; i < menuItems.length; i++) {
+      menuItems[i].classList.add('current-menu');
+    }
 }
 
+if (url.indexOf('challenges') > -1) { 
 document.getElementsByClassName('challenge-content')[0]
         .addEventListener('click', function (event) {
             // do something
         });
-// challenges: show selected
-function changeChallenge(element) {
-    console.log(element)
-    // add filter to all images that are in div class <category-image>
-    for (let item of document.getElementsByClassName('category')) {
-        item.classList.add('not-selected')
-        item.getElementsByTagName('img')[0].classList.add('img-greyed')
-        item.getElementsByTagName('img')[0].classList.remove('img-selected')
-    }
-    element.classList.remove('not-selected')
-
-    element.getElementsByTagName('img')[0].classList.remove('img-greyed')
-    element.getElementsByTagName('img')[0].classList.add('img-selected')
-        
-    // to all sections with class <challenge-content> add d-none
-    for (let item of document.getElementsByClassName('challenge-content')) {
-        console.log(item)
-        item.classList.add('d-none')
-    }
-    // remove d-none of element with id in attribute data-target-id
-    console.log('element', element)
-    let target_id = element.getAttribute('data-target-id')
-    console.log('target_id', target_id)
-    document.getElementById(target_id).classList.remove('d-none')
-    window.history.pushState("object or string", "Title", "#" + target_id);
 }
-
-
