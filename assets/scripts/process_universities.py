c56f34c136352f56e3b8ad90698bce1e6a44b1e4
DanielSzym <d.szymkowiak@gmx.net>
2020-04-19 13:01:29 +0200

    add script to assemble html for universities

---
+++ assets/scripts/process_universities.py
@@ -0,0 +1,33 @@
+# -------------------------------------------------------------------------------------------- #
+# 
+# IMPORTANT:
+# Before using this script, please install the requirements via pip install requirements.txt
+# Run script from project root folder
+# 
+# ABOUT:
+# This script takes a list of university partners and creates html list elements out of it
+# Takes a comma-separated csv file with columns <name> and <link>
+# 
+# -------------------------------------------------------------------------------------------- #
+
+import pandas as pd
+import numpy as np
+import os
+
+df = pd.read_csv('./assets/scripts/universities.csv')
+
+html = ''
+for index, row in df.iterrows():
+    html_list_element = '<a '
+    if df.at[index, 'link'] == '-':
+        # no link provided, don't add href attribute
+        html_list_element += 'target="_blank" rel="noreferrer">'
+    else:
+        html_list_element += 'href="'+str(df.at[index, 'link'])+'" target="_blank" rel="noreferrer">'
+    html_list_element += '<li>'+str(df.at[index, 'name'])+'</li></a>'
+
+    html += html_list_element
+
+# add to file
+with open(os.path.join('./assets/scripts', 'partners_universities.html'), 'w') as unis:
+    unis.write(html)
